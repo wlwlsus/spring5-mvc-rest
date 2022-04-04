f2b165b19064500d2740564b502b8b79c6bf0456
swj <159632_@naver.com>
2022-04-05 03:14:00 +0900

    Feat : Vendor Controller 추가

--- src/main/java/guru/springfamework/controllers/v1/VendorController.java
+++ src/main/java/guru/springfamework/controllers/v1/VendorController.java
@@ -1,7 +1,10 @@
 package guru.springfamework.controllers.v1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import guru.springfamework.api.v1.model.VendorDto;
+import guru.springfamework.api.v1.model.VendorListDto;
+import guru.springfamework.services.VendorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * author : cadqe13@gmail.com
@@ -15,4 +18,45 @@ public class VendorController {
 
 	public static final String BASE_URL = "/api/v1/vendors";
 
+	private final VendorService vendorService;
+
+	public VendorController(VendorService vendorService) {
+		this.vendorService = vendorService;
+	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public VendorListDto getListOfVendors() {
+		return new VendorListDto(vendorService.getAllVendors());
+	}
+
+	@GetMapping({"/{id}"})
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDto getVendorById(@PathVariable Long id) {
+		return vendorService.getVendorById(id);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public VendorDto createNewCustomer(@RequestBody VendorDto vendorDto) {
+		return vendorService.createNewVendor(vendorDto);
+	}
+
+	@PutMapping("{/id}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDto updateVendor(@PathVariable Long id, @RequestBody VendorDto vendorDto) {
+		return vendorService.saveVendorByDto(id, vendorDto);
+	}
+
+	@PatchMapping("{/id}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDto patchVendor(@PathVariable Long id, @RequestBody VendorDto vendorDto) {
+		return vendorService.patchVendor(id, vendorDto);
+	}
+
+	@DeleteMapping("{/id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteVendor(@PathVariable Long id) {
+		vendorService.deleteVendorById(id);
+	}
 }
